b9a3964d9141f2facc73a7b1fb0e6cbfebd45cf7
Julian Ruiz <jnrt96@gmail.com>
2020-04-28 23:37:19 -0700

    registro de usuario

---
+++ bd/conexionDB.go
@@ -0,0 +1,38 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//MongoCN es el objeto de conexion a la BD
+var MongoCN = ConectarDB()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://admin:admin@twitter-9dxli.mongodb.net/test?retryWrites=true&w=majority")
+
+//ConectarDB es la funcion que me permite conectar la BD
+func ConectarDB() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexión exitosa con la bd")
+	return client
+}
+
+//ChequeoConnection es el ping a la BD
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/Julianrt/twittor/bd"
+	"github.com/Julianrt/twittor/handlers"
+)
+
+func main() {
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexion a la DB")
+		return
+	}
+	handlers.Manejadores()
+}
